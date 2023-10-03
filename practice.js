096474f2163d030e435f20481ddb42263870e323
Nova Lindberg <nova.lindberg@elev.ga.ntig.se>
2023-10-03 09:15:18 +0200

    if satser för deras ålder och text för if och else

--- practice.js
+++ practice.js
@@ -13,7 +13,16 @@ var catName2 = "nisse"
   let smulan = "8"
   let nisse = "4"
   controlStructures(element)
-
+if (smulan > 8) {
+    Text = "hon är fortfarande inte jätte gamal"
+} else {
+  Text = "hon kommer ju dö snart hon är så gamal"
+}
+if (nisse > 4) {
+Text = "bäbis nisse"
+} else {
+  Text = "sen när blev han en riktig katt"
+}
   arraysAndObjects(element)
 
   domAndEvents(element)
